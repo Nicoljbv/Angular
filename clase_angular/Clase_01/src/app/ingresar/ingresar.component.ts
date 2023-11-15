3eb9e31aed2f1885bbeaec5dd610241337fcc1c4
Nicoljbv <nicoljbv12@gmail.com>
2023-11-14 23:29:55 -0500

    login & register
    Falta el css de los dos formularios y del dashboard

--- clase_angular/Clase_01/src/app/ingresar/ingresar.component.ts
+++ clase_angular/Clase_01/src/app/ingresar/ingresar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ingresar',
@@ -7,11 +8,40 @@ import { Component } from '@angular/core';
 })
 export class IngresarComponent {
 
-  usuario:String=''
-  clave:String=''
+  existe:Boolean=false
+  claveLocalStorage = 'datosUsuarios';
   constructor() { }
 
-  ingresar(){
+  formularioContacto = new FormGroup({
+    nombre: new FormControl('', [Validators.required]),
+    clave: new FormControl('', [Validators.required])
+  });
 
-
+  
+  submit() {
+    this.existe = false;
+    const datosAlmacenados = localStorage.getItem(this.claveLocalStorage);
+  
+    // Parsea los datos almacenados o inicializa una lista vacía si no hay datos
+    const listaDatos = datosAlmacenados ? JSON.parse(datosAlmacenados) : [];
+  
+    for (const dato of listaDatos) {
+      if (dato.nombre === this.formularioContacto.value.nombre && dato.clave === this.formularioContacto.value.clave) {
+        this.existe = true;
+      }
+    }
+  
+    if (this.existe) {
+      console.log("Bienvenido");
+      console.log(this.formularioContacto.value.nombre);
+      console.log(this.formularioContacto.value.clave);
+      
+      
+    } else {
+      console.log("Usuario o clave incorrectos");
+    }
+  
+    this.formularioContacto.reset();
+  }
+  
 }
--- clase_angular/Clase_01/src/app/registrar/registrar.component.ts
+++ clase_angular/Clase_01/src/app/registrar/registrar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+
 @Component({
   selector: 'app-registrar',
   templateUrl: './registrar.component.html',
@@ -8,16 +9,36 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class RegistrarComponent {
 
+  
   resultado: String=''
+  claveLocalStorage = 'datosUsuarios';
 
   formularioContacto = new FormGroup({
-    nombre: new FormControl('', [Validators.required, Validators.minLength(10)])
+    nombre: new FormControl('', [Validators.required]),
+    apellido: new FormControl('', [Validators.required]),
+    correo: new FormControl('', [Validators.required, Validators.email]),
+    clave: new FormControl('', [Validators.required])
   });
 
   submit() {
-    if (this.formularioContacto.valid)
+    if (this.formularioContacto.valid){
       this.resultado = "Todos los datos son válidos";
-    else
-      this.resultado = "Hay datos inválidos en el formulario";
+      //localStorage.setItem('usuarios', JSON.stringify(this.formularioContacto.value));
+       // Obtén los datos almacenados actualmente en localStorage
+    const datosAlmacenados = localStorage.getItem(this.claveLocalStorage);
+
+    // Parsea los datos almacenados o inicializa una lista vacía si no hay datos
+    const listaDatos = datosAlmacenados ? JSON.parse(datosAlmacenados) : [];
+
+    // Agrega el nuevo conjunto de datos al final de la lista
+    listaDatos.push(this.formularioContacto.value);
+
+    // Guarda la lista actualizada en localStorage
+    localStorage.setItem(this.claveLocalStorage, JSON.stringify(listaDatos));
+    
+      this.formularioContacto.reset();
+    }else{
+      this.resultado = "Hay datos vacios o inválidos en el formulario";
   }
+}
 }
